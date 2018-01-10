350f05f97d95cdb7516c520c660a11a1b46e0a98
Gogs <gogs@fake.local>
2018-01-10 15:34:36 +0800

    bug修复： invoke返回json格式不正确

--- sdk/config.go
+++ sdk/config.go
@@ -5,5 +5,5 @@ const (
 	DEFAULT_ORG_ID              = "Org1"
 	DEFAULT_CONFIG_FILE         = "./fixtures/config/config_test.yaml"
 	DEFAULT_CHANNEL_CONFIG_FILE = "./fixtures/channel/mychannel.tx"
-	CHAIN_CODE_ID               = "dasset14"
+	CHAIN_CODE_ID               = "dasset15"
 )
--- sdk/invoke.go
+++ sdk/invoke.go
@@ -2,12 +2,26 @@ package sdk
 
 import (
 	"encoding/hex"
+	"encoding/json"
+	//"errors"
 	"fmt"
+	//"time"
 
 	"github.com/hyperledger/fabric-sdk-go/api/apitxn"
 )
 
+type InvokeReturnData struct {
+	TxID  string `json:"txid"`
+	Nonce string `json:"nonce"`
+}
+
 func (testSetup *BaseSetupImpl) InvokeChainCode(method, header, content, sign string) (string, error) {
+
+	//	eventID := "test([a-zA-Z]+)"
+
+	//	// Register chaincode event (pass in channel which receives event details when the event is complete)
+	//	notifier := make(chan *apitxn.CCEvent)
+	//	rce := testSetup.ChannelClient.RegisterChaincodeEvent(notifier, testSetup.ChainCodeID, eventID)
 
 	invokeArgs := [][]byte{
 		[]byte(method),
@@ -23,12 +37,44 @@ func (testSetup *BaseSetupImpl) InvokeChainCode(method, header, content, sign st
 	txID, err := testSetup.ChannelClient.ExecuteTx(request)
 	if err != nil {
 		fmt.Printf("Failed to create account: %s\n", err)
+		//testSetup.ChannelClient.UnregisterChaincodeEvent(rce)
 		return "", err
 	}
 
-	jsonResult := fmt.Sprintf(`{"txid":"%s", nonce="%s"}`, txID.ID, hex.EncodeToString(txID.Nonce))
+	//	select {
+	//	case ccEvent := <-notifier:
+	//		fmt.Printf("Received cc event: %s", ccEvent)
+	//		if ccEvent.TxID != txID.ID {
+	//			msg := fmt.Sprintf("CCEvent(%s) and ExecuteTx(%s) transaction IDs don't match", ccEvent.TxID, txID.ID)
+	//			err = errors.New(msg)
+	//			fmt.Println(msg)
+	//			testSetup.ChannelClient.UnregisterChaincodeEvent(rce)
+	//			return "", err
+	//		}
+	//	case <-time.After(time.Second * 20):
+	//		msg := fmt.Sprintf("Did NOT receive CC for eventId(%s)\n", eventID)
+	//		err = errors.New(msg)
+	//		fmt.Println(msg)
+	//		testSetup.ChannelClient.UnregisterChaincodeEvent(rce)
+	//		return "", err
+	//	}
+
+	//	// Unregister chain code event using registration handle
+	//	err = testSetup.ChannelClient.UnregisterChaincodeEvent(rce)
+	//	if err != nil {
+	//		fmt.Printf("Unregister cc event failed: %s", err)
+	//		return "", err
+	//	}
+
+	data := &InvokeReturnData{
+		TxID:  txID.ID,
+		Nonce: hex.EncodeToString(txID.Nonce),
+	}
+	jsonResult, _ := json.Marshal(data)
+
+	//jsonResult := fmt.Sprintf(`{"txid":"%s", nonce="%s"}`, txID.ID, hex.EncodeToString(txID.Nonce))
 	fmt.Printf("result1=%s\n", jsonResult)
-	return jsonResult, nil
+	return string(jsonResult), nil
 }
 
 func (testSetup *BaseSetupImpl) QueryChainCode(method, filter string) (string, error) {
